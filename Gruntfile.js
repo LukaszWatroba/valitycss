224375053c24b789ee84f74b71e82f793cd4f765
Łukasz Wątroba <lukasz.jan.watroba@gmail.com>
2014-12-22 23:15:14 +0100

    change banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,14 +11,16 @@ module.exports = function (grunt) {
     dist: 'dist'
   };
 
-  var banner = '/*!\n'
-          + ' * ValityCss v0.0.3 (http://lukaszwatroba.github.io/valitycss)\n'
-          + ' * Created by Łukasz Wątroba (http://lukaszwatroba.com)\n'
-          + ' * Licensed under MIT (https://github.com/LukaszWatroba/valitycss/blob/master/LICENSE)\n'
-          + ' */\n\n';
+  var banner = '/**\n' +
+      ' * <%= pkg.description %>\n' +
+      ' * @version v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      ' * @link <%= pkg.homepage %>\n' +
+      ' * @author <%= pkg.author %>\n' +
+      ' * @license MIT License, http://www.opensource.org/licenses/MIT\n' +
+      ' */\n';
 
   grunt.initConfig({
-
+    pkg: grunt.file.readJSON('package.json'),
     config: config,
 
     watch: {
